ced87708644291cc8796e927ef8d7a10749bf13c
MohdImranulHoqueLimon <limon1036@gmail.com>
2018-11-30 17:21:20 +0100

    Basic template view

--- src/Controller/AddressController.php
+++ src/Controller/AddressController.php
@@ -2,25 +2,47 @@
 
 namespace App\Controller;
 
+use Entity\Address;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AddressController extends Controller
 {
     /**
-     * @Route("/limon", name="limon")
+     * @Route("/list", name="list_addresses")
      */
-    public function index()
+    public function indexAction()
     {
-        return new JsonResponse(array('result' => 'this is test result' ));
+        /*$address = $this->getDoctrine()
+            ->getRepository(Address::class)->findAll();*/
+        $address = array();
+        return $this->render('address/index.html.twig', array('address' => $address));
     }
 
     /**
-     * @Route("/dhaka", name="dhaka")
+     * @Route("/create", name="create_address")
      */
-    public function dhaka()
+    public function createAction(Request $request)
     {
-        return new JsonResponse(array('result' => 'this is dhaka' ));
+        return $this->render('address/create.html.twig');
+    }
+
+    /**
+     * @Route("/save", name="save_address")
+     */
+    public function saveAction(Request $request)
+    {
+        $address = new Address();
+
+        $address->firstName = $request->get('firstName');
+        $address->lastName = $request->get('lastName');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($address);
+        $em->flush();
+
+        return $this->redirect('list');
     }
 }
