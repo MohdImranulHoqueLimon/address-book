bd6419898f0446c00908e7e0257f6f770cf9953f
MohdImranulHoqueLimon <limon1036@gmail.com>
2018-12-02 00:21:13 +0100

    put value in edit page

---
+++ src/Entity/Address.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $streetNumber;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $zip;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $phoneNumber;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $birthDay;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $email;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getStreetNumber()
+    {
+        return $this->streetNumber;
+    }
+
+    public function setStreetNumber(int $streetNumber)
+    {
+        $this->streetNumber = $streetNumber;
+
+        return $this;
+    }
+
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZip()
+    {
+        return $this->zip;
+    }
+
+    public function setZip(string $zip)
+    {
+        $this->zip = $zip;
+
+        return $this;
+    }
+
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    public function getBirthDay()
+    {
+        return $this->birthDay;
+    }
+
+    public function setBirthDay(string $birthDay)
+    {
+        $this->birthDay = $birthDay;
+
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
